36ff8a63e742d4b4609ff94d9b682743147bcc61
NandawteK <nandawtek@gmail.com>
2023-08-25 19:56:28 +0200

    Add exceptions

--- test/errors.spec.ts
+++ test/errors.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { BadRequestException, HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { AppController } from '../src/app/app.controller';
@@ -22,12 +22,27 @@ describe('AppController', () => {
     })
     const response = await request(app.getHttpServer())
       .get('/v1/healthz')
-      .expect(500)
+      .expect(HttpStatus.INTERNAL_SERVER_ERROR)
 
     expect(response.body.errorId).toBeTruthy()
     expect(response.body.timestamp).toBeTruthy()
     expect(response.body.message).toBe('Test Error')
     expect(response.body.path).toBe('/v1/healthz')
-    expect(response.body.statusCode).toBe(500)
+    expect(response.body.statusCode).toBe(HttpStatus.INTERNAL_SERVER_ERROR)
+  });
+
+  it('Throw Bad Request', async () => {
+    jest.spyOn(AppController.prototype, 'getHealthzResponse').mockImplementation(() => {
+      throw new BadRequestException('Test Error')
+    })
+    const response = await request(app.getHttpServer())
+      .get('/v1/healthz')
+      .expect(HttpStatus.BAD_REQUEST)
+
+    expect(response.body.errorId).toBeTruthy()
+    expect(response.body.timestamp).toBeTruthy()
+    expect(response.body.message).toBe('Test Error')
+    expect(response.body.path).toBe('/v1/healthz')
+    expect(response.body.statusCode).toBe(HttpStatus.BAD_REQUEST)
   });
 });
