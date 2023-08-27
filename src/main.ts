ae8d6a3778634fe87df5efaa460d6f735b7312e0
NandawteK <nandawtek@gmail.com>
2023-08-27 08:46:07 +0200

    add helmet

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import Logger from './shared/infrastructure/logger';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
 
+  app.use(helmet());
+  
   const config = new DocumentBuilder()
     .setTitle('Api Docs')
     .setDescription('Api documentation')
